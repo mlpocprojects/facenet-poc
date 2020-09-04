089e74ee29f90da246eeba787bcb8269335fa7aa
amoljagadambe <amol.jagdambe@gmail.com>
2020-09-04 15:25:57 +0530

    facenet code updated with liveness detection

--- facenet.py
+++ facenet.py
@@ -4,14 +4,14 @@ from multiprocessing.dummy import Pool
 
 K.set_image_data_format('channels_first')
 from cv2 import cv2
-import os
 import glob
-import numpy as np
+from collections import defaultdict
 from numpy import genfromtxt
-import tensorflow as tf
 from fr_utils import *
+from liveness_model import *
 from inception_blocks_v2 import *
 import win32com.client as wincl
+from cv2.data import haarcascades
 import pyttsx3
 
 PADDING = 50
@@ -54,8 +54,23 @@ def triplet_loss(y_true, y_pred, alpha=0.3):
     return loss
 
 
+def isBlinking(history, maxFrames):
+    """ @history: A string containing the history of eyes status
+         where a '1' means that the eyes were closed and '0' open.
+        @maxFrames: The maximal number of successive frames where an eye is closed """
+    for i in range(maxFrames):
+        pattern = '1' + '0' * (i + 1) + '1'
+        if pattern in history:
+            return True
+    return False
+
+
+# load the facnet-model weights
 FRmodel.compile(optimizer='adam', loss=triplet_loss, metrics=['accuracy'])
 load_weights_from_FaceNet(FRmodel)
+
+# load the liveness model
+model = load_model()
 
 
 def prepare_database():
@@ -82,7 +97,11 @@ def webcam_face_recognizer(database):
     cv2.namedWindow("preview")
     vc = cv2.VideoCapture(0)
 
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier(os.path.join(haarcascades, 'haarcascade_frontalface_default.xml'))
+    face_detector = cv2.CascadeClassifier(os.path.join(haarcascades, 'haarcascade_frontalface_alt.xml'))
+    open_eyes_detector = cv2.CascadeClassifier(os.path.join(haarcascades, 'haarcascade_eye_tree_eyeglasses.xml'))
+    left_eye_detector = cv2.CascadeClassifier(os.path.join(haarcascades, 'haarcascade_lefteye_2splits.xml'))
+    right_eye_detector = cv2.CascadeClassifier(os.path.join(haarcascades, 'haarcascade_righteye_2splits.xml'))
 
     while vc.isOpened():
         _, frame = vc.read()
@@ -90,7 +109,7 @@ def webcam_face_recognizer(database):
 
         # We do not want to detect a new identity while the program is in the process of identifying another person
         if ready_to_detect_identity:
-            img = process_frame(img, frame, face_cascade)
+            img = process_frame(img, frame, face_cascade, open_eyes_detector, left_eye_detector, right_eye_detector)
 
         key = cv2.waitKey(100)
         cv2.imshow("preview", img)
@@ -100,10 +119,11 @@ def webcam_face_recognizer(database):
     cv2.destroyWindow("preview")
 
 
-def process_frame(img, frame, face_cascade):
+def process_frame(img, frame, face_cascade, open_eyes_detector, left_eye_detector, right_eye_detector):
     """
     Determine whether the current frame contains the faces of people from our database
     """
+    eyes_detected = defaultdict(str)
     global ready_to_detect_identity
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -122,6 +142,79 @@ def process_frame(img, frame, face_cascade):
 
         if identity is not None:
             identities.append(identity)
+
+        face = frame[y:y + h, x:x + w]
+        gray_face = gray[y:y + h, x:x + w]
+        # Eyes detection
+        # check first if eyes are open (with glasses taking into account)
+        open_eyes_glasses = open_eyes_detector.detectMultiScale(
+            gray_face,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+        # if open_eyes_glasses detect eyes then they are open
+        if len(open_eyes_glasses) == 2:
+            eyes_detected[identity] += '1'
+            for (ex, ey, ew, eh) in open_eyes_glasses:
+                cv2.rectangle(face, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+        # otherwise try detecting eyes using left and right_eye_detector
+        # which can detect open and closed eyes
+        else:
+            # separate the face into left and right sides
+            left_face = frame[y:y + h, x + int(w / 2):x + w]
+            left_face_gray = gray[y:y + h, x + int(w / 2):x + w]
+
+            right_face = frame[y:y + h, x:x + int(w / 2)]
+            right_face_gray = gray[y:y + h, x:x + int(w / 2)]
+
+            # Detect the left eye
+            left_eye = left_eye_detector.detectMultiScale(
+                left_face_gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+
+            # Detect the right eye
+            right_eye = right_eye_detector.detectMultiScale(
+                right_face_gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+
+            eye_status = '1'  # we suppose the eyes are open
+
+            # For each eye check wether the eye is closed.
+            # If one is closed we conclude the eyes are closed
+            for (ex, ey, ew, eh) in right_eye:
+                color = (0, 255, 0)
+                pred = predict(right_face[ey:ey + eh, ex:ex + ew], model)
+                if pred == 'closed':
+                    eye_status = '0'
+                    color = (0, 0, 255)
+                cv2.rectangle(right_face, (ex, ey), (ex + ew, ey + eh), color, 2)
+            for (ex, ey, ew, eh) in left_eye:
+                color = (0, 255, 0)
+                pred = predict(left_face[ey:ey + eh, ex:ex + ew], model)
+                if pred == 'closed':
+                    eye_status = '0'
+                    color = (0, 0, 255)
+                cv2.rectangle(left_face, (ex, ey), (ex + ew, ey + eh), color, 2)
+            eyes_detected[identity] += eye_status
+
+        # Each time, we check if the person has blinked
+        # If yes, we display its name
+        if isBlinking(eyes_detected[identity], 3):
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # Display name
+            y = y - 15 if y - 15 > 15 else y + 15
+            cv2.putText(frame, identity, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
 
     if identities != []:
         cv2.imwrite('example.png', img)
@@ -174,7 +267,7 @@ def who_is_it(image, database, model):
         # Compute L2 distance between the target "encoding" and the current "emb" from the database.
         dist = np.linalg.norm(db_enc - encoding)
 
-        print('distance for %s is %s' % (name, dist))
+        # print('distance for %s is %s' % (name, dist))
 
         # If this distance is less than the min_dist, then set min_dist to dist, and identity to name
         if dist < min_dist:
@@ -212,10 +305,3 @@ if __name__ == "__main__":
     database = prepare_database()
     webcam_face_recognizer(database)
 
-# ### References:
-# 
-# - Florian Schroff, Dmitry Kalenichenko, James Philbin (2015). [FaceNet: A Unified Embedding for Face Recognition and Clustering](https://arxiv.org/pdf/1503.03832.pdf)
-# - Yaniv Taigman, Ming Yang, Marc'Aurelio Ranzato, Lior Wolf (2014). [DeepFace: Closing the gap to human-level performance in face verification](https://research.fb.com/wp-content/uploads/2016/11/deepface-closing-the-gap-to-human-level-performance-in-face-verification.pdf) 
-# - The pretrained model we use is inspired by Victor Sy Wang's implementation and was loaded using his code: https://github.com/iwantooxxoox/Keras-OpenFace.
-# - Our implementation also took a lot of inspiration from the official FaceNet github repository: https://github.com/davidsandberg/facenet 
-#
